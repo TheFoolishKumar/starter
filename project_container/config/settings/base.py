e975b7e34e435bf7b2602d5aeaa0a589da9264c2
Unknown <daddu.coder@gmail.com>
2017-12-03 10:00:42 +0530

    Add templates and static directory
    To facilitate easy template and static files viewing, keep them all in the respective directory.

--- project_container/config/settings/base.py
+++ project_container/config/settings/base.py
@@ -22,10 +22,6 @@ def get_secret(setting, secrets=secrets):
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_secret("SECRET_KEY")
 
